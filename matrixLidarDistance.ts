b963a452125f9b30a12220d26c754248e9ce3703
zhangtang <2907578142@qq.com>
2024-09-11 14:04:52 +0800

    init

--- matrixLidarDistance.ts
+++ matrixLidarDistance.ts
@@ -1,38 +1,76 @@
 
-/**
-* 使用此文件来定义自定义函数和图形块。
-* 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
-*/
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
 
-/**
- * Custom blocks
- */
-//% weight=100 color=#0fbc11 icon=""
-namespace custom {
+//% block="Matrix LiDAR Distance"
+//% weight=100 color=#5b3fe8 icon="\uf022"
+//% groups="['Distance measurement', 'Obstacle Avoidance']"
+namespace matrixLidarDistance {
+
+    export enum ObstacleSide {
+        //% block="Left"
+        Left = 1,
+        //% block="Front"
+        Front = 2,
+        //% block="Right"
+        Right = 3,
+    }
+    
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * For the setting of obstacle avoidance distance, the sensor will make suggestions for obstacle avoidance based on the distance value of detecting obstacles in front of it.
+     * @param distance to distance ,eg: 10
      */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
+
+    //% block="Obstacle avoidance distance %distance"
+    //% distance.min=10 distance.max=50 distance.defl=10
+    //% weight=90
+    export function setObstacleDistance(distance: number) {
+        
     }
 
     /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * Check if the detected distance value has reached the critical distance for an imminent collision, which is a fixed value of 10cm. If this distance is detected, issue a warning.
      */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+
+    //% block="Emergency warning?"
+    //% weight=80
+    export function emergencyWarning(): boolean {
+        return false;
     }
+
+    /**
+     * The API will provide steering suggestions for the car based on the data detected in front of the vehicle.
+    */
+
+    //% block="Suggestions for obstacle avoidance"
+    //% weight=70
+    export function obstacleSuggestion(): number {
+        return 1;
+    }
+
+    /**
+     * By the system's comprehensive calculation, the distance values of obstacles in the current left, center, and right directions are detected in real time.
+     * @param side to side ,eg: ObstacleSide.Left
+    */
+
+    //% block="Distance detection %side"
+    //% weight=60
+    export function getObstacleDistance(side: ObstacleSide): number {
+        return 1;
+    }
+
+    /**
+     * Specify one of the point output data, which is used to simplify the difficulty of project development and reduce the amount of data.
+     * @param x to x ,eg: 3
+     * @param y to y ,eg: 3
+    */
+
+    //% block="matrix Output x %x y %y"
+    //% weight=50
+    //% x.min=0 x.max=7 x.defl=3
+    //% y.min=0 y.max=7 y.defl=3
+    export function matrixPointOutput(x: number, y: number): number {
+        return 1;
+    }
+
 }
+
